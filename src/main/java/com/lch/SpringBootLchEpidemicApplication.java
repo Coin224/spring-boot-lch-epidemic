d6be2bb4a46c2bdaac8778d9e9e140c93bb21849
Coin224 <821504961@qq.com>
2020-05-11 16:52:10 +0800

    Initial commit

---
+++ src/main/java/com/lch/SpringBootLchEpidemicApplication.java
@@ -0,0 +1,13 @@
+package com.lch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLchEpidemicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLchEpidemicApplication.class, args);
+    }
+
+}
